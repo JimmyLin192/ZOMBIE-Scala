7e3ae8e4a44d523ad00ff2a5146637f866c8a5f0
jimmylin192 <linxin@gmail.com>
2016-11-13 04:51:30 -0600

    a new framework

--- ScalaZombie.scala
+++ ScalaZombie.scala
@@ -1,2 +1,72 @@
 /* Scala Implementation of Zombie Language */
 
+class ScalaZombie {
+
+    /* Define keywords: Entity Declaration */
+    object ZOMBIE {
+        def apply(name: String) {
+
+        }
+    }
+    object GHOST {
+        def apply(name: String) {
+
+        }
+    }
+    object VAMPIRE {
+        def apply(name: String) {
+
+        }
+    }
+    object DEMON {
+        def apply(name: String) {
+
+        }
+    }
+    object DJINN {
+        def apply(name: String) {
+
+        }
+    }
+    /* Define keywords: Task */
+    def REMEMBER (num: Int) {
+
+    }
+    def MOAN (num: Int) {
+
+    }
+    def SAY (text: String) {
+
+    }
+    /* Define keywords: Flow Control  */
+    /* Sequential */
+    def SUMMON {
+
+    }
+    def BIND {
+
+    }
+    def DISTURB {
+
+    }
+    /* Repetition */
+    def SHAMBLE {
+        
+    }
+    def UNTIL {
+        
+    }
+    def STUMBLE {
+
+    }
+    /* Condition */
+    def TASTE {
+
+    }
+    def GOOD {
+
+    }
+    def BAD {
+
+    }
+}
