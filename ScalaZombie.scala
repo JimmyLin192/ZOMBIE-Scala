d38cf45bb8716f47b2003eac768e23f7e029462a
jimmylin192 <linxin@gmail.com>
2016-11-13 05:23:28 -0600

    more on zombie with small set of definition

--- ScalaZombie.scala
+++ ScalaZombie.scala
@@ -1,50 +1,89 @@
 /* Scala Implementation of Zombie Language */
+import scala.collection.mutable
 
 class ScalaZombie {
 
-    /* Define keywords: Entity Declaration */
-    object ZOMBIE {
-        def apply(name: String) {
+    val curEntIty : String = None
+    val memories = new mutable.HashMap[String, Int]()
 
+    val canInitSummon : Boolean = false
+    val canExecTask : Boolean = false
+
+    /* Define keywords: EntIty Declaration */
+    object ZOMBIE {
+        def apply(entIty: String) {
+            memories(entIty) = 0
+            canInitSummon = true
         }
     }
     object GHOST {
-        def apply(name: String) {
+        def apply(entIty: String) {
 
         }
     }
     object VAMPIRE {
-        def apply(name: String) {
+        def apply(entIty: String) {
 
         }
     }
     object DEMON {
-        def apply(name: String) {
+        def apply(entIty: String) {
 
         }
     }
     object DJINN {
-        def apply(name: String) {
+        def apply(entIty: String) {
 
         }
     }
     /* Define keywords: Task */
     def REMEMBER (num: Int) {
-
+        REMEMBER(curEntIty, num)
     }
-    def MOAN (num: Int) {
-
+    def REMEMBER (entIty: String, num: Int) {
+        if (!canExecTask) {
+            throw new RuntimeException("It is not the time yet to call REMEMBER.")
+        }
+        if (!memories.exists(entIty)) {
+           throw new RuntimeException("Cannot remember sth for a non-existent entIty.")
+        }
+        memories(entIty) = num
+    }
+    def MOAN (entIty: String) {
+        if (!canExecTask) {
+            throw new RuntimeException("It is not the time yet to call MOAN.")
+        }
+        if (!memories.exists(entIty)) {
+           throw new RuntimeException("Cannot moan sth for a non-existent entIty.")
+        }
+        memories(entIty)
     }
     def SAY (text: String) {
+        SAY(curEntIty, text)
+    }
+    def SAY (entIty: String, text: String) {
+        if (!canExecTask) {
+            throw new RuntimeException("It is not the time yet to call SAY.")
+        }
+        if (!memories.exists(entIty)) {
+           throw new RuntimeException("Cannot say sth for a non-existent entIty.")
 
+        println(text)
     }
     /* Define keywords: Flow Control  */
     /* Sequential */
     def SUMMON {
-
+        if (canInitSummon) {
+            canExecTask = true
+            canInitSummon = false
+        }
     }
     def BIND {
-
+        if (canExecTask) {
+            canExecTask = false
+        } else {
+            throw new RuntimeException("It is not the time yet to call BIND.")
+        }
     }
     def DISTURB {
 
