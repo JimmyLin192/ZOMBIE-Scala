c449932ef3fe339600a619854201decb85ca85fc
Spencer Bull <spencerbull2554@gmail.com>
2016-11-30 15:59:03 -0600

    update task example

--- zscripts/HelloWorldAnimateTask.scala
+++ zscripts/HelloWorldAnimateTask.scala
@@ -2,6 +2,7 @@ object HelloWorldAnimateTask extends ScalaZombie {
     def main (args: Array[String]): Unit = {
 ZOMBIE("HelloWorld")
 SUMMON
+    SAY("Hellow without a task")
     TASK("SayHello")
         SAY("Hello World!")
     ANIMATE
